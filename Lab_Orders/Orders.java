de9e98502c1a95cf131bbc1ba2e6ec17365b50cd
Bryan Marchal <hewhoeatsthebagel@gmail.com>
2017-01-25 08:35:49 -0600

    Add files via upload

---
+++ Lab_Orders/Orders.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+/public class Item{
+
+  private String name;
+  private Double price;
+  private Integer quantity;
+
+  public String setName(String name){
+    name = s;
+  }
+  public Double setPrice(Double price){
+
+  }
+  public int setQuantity(int quantity){
+
+  }
+}*/
+
+public class Order{
+
+  public static void main(String args[]){
+
+    if(args.length < 1){
+      System.err.println("Must have at least one argument.");
+      System.exit(1);
+    }
+  }
+
+
+}
