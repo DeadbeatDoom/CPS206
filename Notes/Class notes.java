3a846a7b8be309495ce12e8049ed11abadc44341
Bryan Marchal <hewhoeatsthebagel@gmail.com>
2017-01-31 08:35:02 -0600

    Class notes

---
+++ Notes/Class notes.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+private class GenericExample{
+  public static void main (string...args){
+    ArrayList <string> list = new ArrayList();
+    System.out.println(list.size());
+    list.add("Bryan Marchal");
+    System.out.println(list.size());
+    list.add(0, "Marchal");
+    System.out.println(list);
+  }
+}
+
+
+// <E extends shape> Sets upper bound
+// findAverage A rec()
+// (?superInteger) Sets lower bound
+// (E extends Comparable(?super E)) Sets upper and lower bounds
+
+//
