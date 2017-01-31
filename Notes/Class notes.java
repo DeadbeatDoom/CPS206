dc7180c8bb48ea19971a13d226b13e63cad7652a
Bryan Marchal <hewhoeatsthebagel@gmail.com>
2017-01-31 08:40:51 -0600

    Contains Method

--- Notes/Class notes.java
+++ Notes/Class notes.java
@@ -1,12 +1,16 @@
-import java.util.ArrayList;
-private class GenericExample{
-  public static void main (string...args){
+import java.io.*;
+import java.util.*;
+public class List{
+  public static void main (string[] args){
     ArrayList <string> list = new ArrayList();
     System.out.println(list.size());
     list.add("Bryan Marchal");
     System.out.println(list.size());
     list.add(0, "Marchal");
     System.out.println(list);
+    System.out.println(list.contains("Kulis"));//F
+    System.out.println(list.contains("Marchal"));//T?F
+
   }
 }
 
